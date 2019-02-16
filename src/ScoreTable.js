dc051ec05af1c06805d002b1a8307147d7f02711
Orlowsky <orlowski.aleksander@gmail.com>
2019-02-16 16:48:26 +0100

    ScoreTable 1

---
+++ src/ScoreTable.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ScoreTable = () => {
+  return( 
+  <div>
+      <div>
+    <DateAndTime /> 
+    <TeamLogo />
+    <TeamLogo />
+    <Score />
+    </div>
+
+</div>
+);
+
+}
+
+export default ScoreTable;
